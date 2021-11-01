bc2ac261e04cdf794d46bab7455b1bbba0b24fc9
Bobbie Soedirgo <bobbie@soedirgo.dev>
2021-11-01 12:06:52 +0800

    feat: add new table

---
+++ supabase/database/tables/public.t.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS public.t
+(
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.t
+    OWNER to postgres;
+
+GRANT ALL ON TABLE public.t TO anon;
+
+GRANT ALL ON TABLE public.t TO authenticated;
+
+GRANT ALL ON TABLE public.t TO postgres;
+
+GRANT ALL ON TABLE public.t TO service_role;
